0348b4a47d18c261d9766dae27c276ab2523031c
Daniel194 <lungu.daniel94@gmail.com>
2017-04-18 23:32:13 +0300

    Home Page - Logout

--- src/main/webapp/home/home.component.js
+++ src/main/webapp/home/home.component.js
@@ -5,12 +5,18 @@
 
 angular.module('libraryApp').component('home', {
     templateUrl: 'home/home.template.html',
-    controller: ['$scope', '$location',
-        function HomeController($scope, $location) {
+    controller: ['$scope', '$location', '$http', 'UserAuthSharedService',
+        function HomeController($scope, $location, $http, UserAuthSharedService) {
 
             $scope.goTo = function (newpath) {
                 $location.path(newpath);
-            }
+            };
+
+            $scope.logout = function () {
+                $http.post('/user/logout', {}).finally(function () {
+                    UserAuthSharedService.logout();
+                });
+            };
 
         }
     ]
