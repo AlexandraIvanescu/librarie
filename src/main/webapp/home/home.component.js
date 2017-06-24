e449a9fed4d4fd20f9b40d4e889a29908570d98c
Daniel194 <lungu.daniel94@gmail.com>
2017-06-24 20:13:42 +0300

    Home

--- src/main/webapp/home/home.component.js
+++ src/main/webapp/home/home.component.js
@@ -5,8 +5,8 @@
 
 angular.module('libraryApp').component('home', {
     templateUrl: 'home/home.template.html',
-    controller: ['$scope', '$location', '$http',
-        function HomeController($scope, $location, $http) {
+    controller: ['$scope', '$location', '$http', '$rootScope', '$mdPanel',
+        function HomeController($scope, $location, $http, $rootScope, $mdPanel) {
 
             $scope.labels_book = ["Numar de carti", "Carti imprumutate", "Carti ce trebuiesc returnate"];
 
@@ -49,6 +49,82 @@ angular.module('libraryApp').component('home', {
             }
 
             getSubscriberStatistics();
+
+            $scope.newBook = function () {
+                var position = $mdPanel.newPanelPosition()
+                    .absolute()
+                    .center();
+
+                var config = {
+                    attachTo: angular.element(document.body),
+                    template: '<add-book></add-book>',
+                    hasBackdrop: true,
+                    panelClass: 'new-post',
+                    position: position,
+                    clickOutsideToClose: true,
+                    escapeToClose: true,
+                    disableParentScroll: true,
+                    trapFocus: true
+                };
+
+                $mdPanel.open(config).then(function (result) {
+                    $rootScope.panelRef = result;
+                });
+
+                $location.path('/books');
+
+            };
+
+            $scope.newSubscriber = function () {
+                var position = $mdPanel.newPanelPosition()
+                    .absolute()
+                    .center();
+
+                var config = {
+                    attachTo: angular.element(document.body),
+                    template: '<add-subscriber></add-subscriber>',
+                    hasBackdrop: true,
+                    panelClass: 'new-post',
+                    position: position,
+                    clickOutsideToClose: true,
+                    escapeToClose: true,
+                    disableParentScroll: true,
+                    trapFocus: true
+                };
+
+                $mdPanel.open(config).then(function (result) {
+                    $rootScope.panelRef = result;
+                });
+
+                $location.path('/subscribers');
+
+            };
+
+
+            $scope.newCategory = function () {
+                var position = $mdPanel.newPanelPosition()
+                    .absolute()
+                    .center();
+
+                var config = {
+                    attachTo: angular.element(document.body),
+                    template: '<add-category></add-category>',
+                    hasBackdrop: true,
+                    panelClass: 'new-post',
+                    position: position,
+                    clickOutsideToClose: true,
+                    escapeToClose: true,
+                    disableParentScroll: true,
+                    trapFocus: true
+                };
+
+                $mdPanel.open(config).then(function (result) {
+                    $rootScope.panelRef = result;
+                });
+
+                $location.path('/settings');
+
+            };
 
         }
     ]
